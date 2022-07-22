8cb8a4ccb444e1cf224f2beec410f16c76e7ee59
maguireong <maguire2622@yahoo.com.sg>
2022-07-22 10:23:08 +0800

    Add page transition loader

--- components/MainTemplate.tsx
+++ components/MainTemplate.tsx
@@ -5,6 +5,8 @@ import { MdOutlineVideoLibrary, MdSubscriptions } from "react-icons/md";
 import { useRouter } from "next/router";
 import { SidePanel } from "./SidePanel";
 import Head from "next/head";
+import { useShowPageTransitionLoader } from "../pageTransitions";
+import { CircularProgress } from "@mui/material";
 
 export function MainTemplate({ children }: { children: ReactNode }) {
   const router = useRouter();
@@ -37,15 +39,24 @@ export function MainTemplate({ children }: { children: ReactNode }) {
     },
   ];
   const currentTab = tabs.find((tab) => tab.link.includes(pathname));
+  const showPageTransitionLoader = useShowPageTransitionLoader();
   return (
     <>
       <Head>
         <title>Youtube Profile</title>
         <link rel="icon" href="/youtube.ico" />
       </Head>
+
       <main className="flex bg-gray-800">
         <SidePanel tabs={tabs} currentTab={currentTab} />
-        {children}
+        {showPageTransitionLoader ? (
+          <div className="flex space-x-2 text-purple-400 h-screen w-full items-center justify-center">
+            <CircularProgress color="secondary" />
+            <div>Loading</div>
+          </div>
+        ) : (
+          children
+        )}
       </main>
     </>
   );
---
+++ pageTransitions.ts
@@ -0,0 +1,78 @@
+/**
+ * Default Next.js behavior with getServerSideProps is whenever we navigate to a page,
+ * the app has no UI changes until the props are received. This makes it look like the
+ * application is frozen. This module provides tooling to handle page transition scenarios.
+ * https://github.com/vercel/next.js/discussions/32243
+ *
+ * Also see a good discussion in our PR 831:
+ * https://github.com/HeadsUpData/Kimono/pull/831#discussion_r884986903
+ */
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+// The more we give, the more often we will not show the spinner, which is good.
+// But 100ms is considered quite noticable in response to a user’s action
+const justStartedDelay = 75;
+
+type TransitionState =
+  | { type: 'loaded' }
+  | { type: 'loading'; destination: string; justStarted: boolean };
+
+function useTransitionState(): TransitionState {
+  const [transitionState, setTransitionState] = useState<TransitionState>({ type: 'loaded' });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    const start = (url: string) => {
+      setTransitionState({ type: 'loading', destination: url, justStarted: true });
+
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        setTransitionState((prev) =>
+          prev.type === 'loading' && prev.destination === url
+            ? { ...prev, justStarted: false }
+            : prev,
+        );
+      }, justStartedDelay);
+    };
+
+    const end = () => setTransitionState({ type: 'loaded' });
+
+    router.events.on('routeChangeStart', start);
+    router.events.on('routeChangeComplete', end);
+    router.events.on('routeChangeError', end);
+
+    return () => {
+      clearTimeout(timeout);
+      router.events.off('routeChangeStart', start);
+      router.events.off('routeChangeComplete', end);
+      router.events.off('routeChangeError', end);
+    };
+  }, [router]);
+
+  return transitionState;
+}
+
+/**
+ * Returns current path, taking into account page transitions.
+ * If the user clicks on a link, this will immediately be returning the new path.
+ */
+export function useCurrentPathWithTransitions(): string {
+  const router = useRouter();
+  const state = useTransitionState();
+  if (state.type === 'loading' && !state.justStarted) return state.destination;
+
+  // Note that router.pathname gives a path with slugs not the actual pathname (e.g. `/campaigns/[id]`)
+  return new URL(router.asPath, 'https://example.com').pathname;
+}
+
+/**
+ * Tells whether or not to show a loader for page navigation
+ */
+export function useShowPageTransitionLoader(): boolean {
+  const state = useTransitionState();
+  return state.type === 'loading' && !state.justStarted;
+}
