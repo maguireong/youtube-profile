02bc1b8b3c864e2968d4b225ba93fbe60d73e598
maguireong <maguire2622@yahoo.com.sg>
2022-07-31 22:22:57 +0800

    Add production configs

--- configs.ts
+++ configs.ts
@@ -4,7 +4,9 @@ export const GOOGLE_CLIENT_ID =
 export const GOOGLE_CLIENT_SECRET = "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";
 export const GOOGLE_REDIRECT_URI = isDevelopment
   ? "http://localhost:3000/api/oauth2"
-  : "";
+  : "https://youtube-profile.herokuapp.com/api/oauth2";
 export const PORT = "3000";
 
-export const BASE_URL = isDevelopment ? "http://localhost:3000" : "";
+export const BASE_URL = isDevelopment
+  ? "http://localhost:3000"
+  : "https://youtube-profile.herokuapp.com";
