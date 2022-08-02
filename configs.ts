db4f01141041faf176980ab994e891b528a7d2b1
maguireong <maguire2622@yahoo.com.sg>
2022-08-02 16:41:53 +0800

    Add configs

--- configs.ts
+++ configs.ts
@@ -1,7 +1,14 @@
 export const isDevelopment = process.env.ENV === "development";
 export const NODE_ENV = process.env.ENV;
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
-export const PORT = process.env.PORT;
-export const BASE_URL = process.env.BASE_URL;
+export const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID ??
+  "1034281273215-1m50l65qceaeueuc3h0aepqbaepccod7.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET =
+  process.env.GOOGLE_CLIENT_SECRET ?? "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";
+export const GOOGLE_REDIRECT_URI = isDevelopment
+  ? process.env.GOOGLE_REDIRECT_URI
+  : "https://youtube-profile.herokuapp.com/api/oauth2";
+export const PORT = "3000";
+export const BASE_URL = isDevelopment
+  ? process.env.BASE_URL
+  : "https://youtube-profile.herokuapp.com";
