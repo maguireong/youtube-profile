ce86aa7f78b02e716f1d4d68fa46b51d2507fd46
maguireong <maguire2622@yahoo.com.sg>
2022-08-02 14:49:37 +0800

    Update subscription detailed view

--- pages/subscriptions/[id].tsx
+++ pages/subscriptions/[id].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { MainTemplate } from "../../components";
+import { MainTemplate, Shimmer } from "../../components";
 import { Subscription } from "../../model";
 import { EmptyVideoPage, SubscriptionDetailView } from "../../views";
 import { getSubscriptionData } from "../../youtube";
@@ -22,7 +22,12 @@ export default function SubscriptionDetail() {
     <MainTemplate>
       <div className="h-screen w-full flex items-center justify-center">
         {sub === "loading" ? (
-          <div className="flex flex-col gap-y-4 items-center br4-skeleton"></div>
+          <div className="animate-pulse flex flex-col justify-center items-center space-y-4">
+            <Shimmer rounded className="w-44 h-44" />
+            <Shimmer className="h-6 w-36 rounded-md" />
+            <Shimmer className="h-14 w-60 rounded-md" />
+            <Shimmer className="h-4 w-28 rounded-md" />
+          </div>
         ) : sub ? (
           <SubscriptionDetailView subscription={sub} />
         ) : (
--- views/SubscriptionDetailView.tsx
+++ views/SubscriptionDetailView.tsx
@@ -1,14 +1,23 @@
+import classNames from "classnames";
 import dayjs from "dayjs";
 import { Subscription } from "../model";
+import { useWindowWidth } from "../useWindowWidth";
 
 export function SubscriptionDetailView({
   subscription,
 }: {
   subscription: Subscription;
 }) {
+  const isMobileView = useWindowWidth() < 1290;
   const dateSubscript = dayjs(subscription?.subscriptAt).format("DD/MM/YYYY");
+
   return (
-    <div className="flex flex-col w-1/3 items-center gap-y-4">
+    <div
+      className={classNames(
+        isMobileView ? "" : " w-1/3",
+        "flex flex-col text-center items-center gap-y-4"
+      )}
+    >
       <img
         className="rounded-full"
         alt="Subcription thumbnail"
@@ -16,13 +25,28 @@ export function SubscriptionDetailView({
         height="40%"
         width="40%"
       />
-      <div className="font-semibold text-4xl text-white">
+      <div
+        className={classNames(
+          isMobileView ? "text-2xl" : "text-4xl",
+          "font-semibold  text-white"
+        )}
+      >
         {subscription.title}
       </div>
-      <div className="space-x-2 text-white flex items-center">
+      <div
+        className={classNames(
+          isMobileView ? "text-sm" : "",
+          "text-white flex items-center"
+        )}
+      >
         <div>{subscription.description}</div>
       </div>
-      <div className="space-x-2 text-white flex items-center">
+      <div
+        className={classNames(
+          isMobileView ? "text-sm" : "",
+          "space-x-2 text-white flex items-center"
+        )}
+      >
         <div>Subscribed at:</div>
         <div>{dateSubscript}</div>
       </div>
